79e1aec15fe140ed798bdea81a4aabf2411168b8
Louis-Felix Beland <louisfelix95@hotmail.com>
2020-09-29 13:33:28 -0400

    elevator and column working and logging

--- Residential_Controller.js
+++ Residential_Controller.js
@@ -1,101 +1,79 @@
-var elevatorAmount = 2;
-var floorAmount = 10;
-var minimumFloor = 1;
-var maximumFloor = 10;
-var direction = "none";
-//var currentDirection = "none";
-let requestedFloor = 3;
 
+class Column {
+    constructor(floorAmount, elevatorAmount) {
+        console.log('Column constructor', floorAmount);
+        this.floorAmount = floorAmount;
+        this.elevatorAmount = elevatorAmount;
+        this.elevatorList=[];
+        this.callButtonList=[];
 
-function Column (floorAmount, minimumFloor, maximumFloor) {
-    this.floorAmount = floorAmount;
-    this.minimumFloor = minimumFloor;
-    this.maximumFloor = maximumFloor;
-};
-
-var column1 = new Column (10, 1, 10);
-
-
-function CallButton (directionUp, directionDown) {
-   // this.minimumFloor = minimumFloor;
-   // this.maximumFloor = maximumFloor;
-   // this.floorAmount = floorAmount;
-    this.directionUp = directionUp;
-    this.directionDown = directionDown;
+        
+        var i=1;
+        while(  i <= elevatorAmount ) {
+            console.log('CREATE AN ELEVATOR', i);
+            let elevator = new Elevator (i, "idle", 2, this.floorAmount);
+            this.elevatorList.push(elevator);
+            i = i+1;
+        }
+        // call Button List
+        
+        for (var i = 1; i <= this.floorAmount; i++){
             
-};
-
-if (currentFloor = 10){
-    var topCallButon = new CallButton ("none","DOWN");
-
-};
-if (currentFloor = 1){
-    var bottomCallButton = new CallButton ("UP","none");
-};
-if (currentFloor = [2,3,4,5,6,7,8,9]){
-    var floorCallButton = new CallButton ("UP", "DOWN")
-};
-
-
-
-var requestedFloor = 3;
-var direction = "UP";
-
-function requestElevator(requestedFloor , direction ) {
-   
-    console.log('requestElevator called');
-    console.log('requestedFloor', requestedFloor);
-    console.log('direction', direction);
+            if(i === 1){
+            this.callButtonList.push(new CallButton ( "up", "off"));
+        }   if (i === 10){
+            this.callButtonList.push(new CallButton ( "down", "off"));
+        }   else {
+            this.callButtonList.push(new CallButton ( "up", "off"));
+            this.callButtonList.push(new CallButton ( "down", "off"));
+        }
+            
+        }
+        console.table(this.callButtonList);
+        
+    }
 }
 
-requestElevator(3, "UP");
+class Elevator {
+    constructor (status, currentFloor, floorAmount) {
+        this.status = status;
+        this.currentFloor = currentFloor;
+        this.floorAmount = floorAmount;
+        this.floorButtonList=[];
+        
 
-function Elevator (currentDirection , currentFloor){
-    this.currentDirection = currentDirection;
-    this.currentFloor =currentFloor;
-    
-};
+        // floor selection button
+        console.log("FLOOR SELECTION BUTTON LIST");
 
-var elevatorA = new Elevator ("none", 2 );
-var elevatorB = new Elevator ("none", 6 );
+        for(var i = 1; i <= floorAmount; i++){
+            
+            this.floorButtonList.push(new FloorButton (i, "off", 1));
+        }
+        console.table(this.floorButtonList);
 
-function createFloorIndicatorList(elevatorAmount, currentFloor){
-
-};
-
-function findBestElevator(requestedFloor, currentDirection, currentFloor) {
-    if (currentDirection = "DOWN") {
-        requestedFloor < currentFloor
-
-    } 
-    else if (currentDirection = "UP"){
-        requestedFloor > currentFloor
     }
-    return bestElevator
-};
 
-//function initialState(callButton, currentFloor, elevatorStatus) {
+}
 
-//};
+class CallButton {
+    constructor (direction, status){
+        this.direction = direction;
+        this.status = status;
 
-//function createRequestList(requestedFloor){
+        
+        
+    }
 
-//};
+}
 
-function FLoorSelectionButton (floorAmount){
-    this.floorAmount = floorAmount;
-};
+class FloorButton {
+    constructor (floorid, status){
+        this.status=status;
+        this.floorid= floorid;
+    }
 
-for(floorAmount = 1; floorAmount <= 10; floorAmount++){
-    var floorSelectionButton = new FLoorSelectionButton(1)
-};
+}
 
 
-function moveElevator(currentDirection, requestedFloor, currentFloor){
-
-};
-
-function openDoors(doorStatus, currentFloor){
-
-};
-
+var column1 = new Column(10, 2);
+var elevator1 = new Elevator("Idle", 2, 10);
