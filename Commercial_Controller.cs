ff7f3f4094d032f110f6f389cde2a08bf19148ab
Louis-Felix Beland <louisfelix95@hotmail.com>
2020-10-05 11:27:07 -0400

    Added instantiation of elevators in a column

--- Commercial_Controller.cs
+++ Commercial_Controller.cs
@@ -8,11 +8,23 @@ namespace Rocket_Elevator_Commercial_Controller
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Battery battery = new Battery(4, 66, 5, -6);
+            Battery battery = new Battery(4, 66, 5, -6, 60);
 
             foreach (Column col in battery.columnList) {
+
+                foreach (Elevator elevator in battery.elevatorList){
+                    Console.WriteLine("ELEVATOR Instanced Status");
+                    Console.WriteLine(elevator.status);
+                }
+                Console.WriteLine("Column Instanced");
                 Console.WriteLine(col.id);
             }
+            Console.WriteLine("Call Button ID AND STATUS");
+            foreach (CallButton call_button in battery.callbuttonList){
+                Console.WriteLine(call_button.call_button_id);
+            }
+            
+
         }
     }
 
@@ -23,27 +35,36 @@ namespace Rocket_Elevator_Commercial_Controller
         int number_of_elevators_per_column;
         int floor_amount;
         int battery_lowest_floor;
+        int battery_maximum_floor;
         public List<Column> columnList = new List<Column>();
+        public List<CallButton> callbuttonList = new List<CallButton>();
+        public List<Elevator> elevatorList = new List<Elevator>();
 
-        public Battery(int _column_amount, int _floor_amount, int _amount_of_elevator_per_column, int _battery_lowest_floor  )
+
+
+        public Battery(int _column_amount, int _floor_amount, int _amount_of_elevator_per_column, int _battery_lowest_floor, int _battery_maximum_floor )
         {
             column_amount = _column_amount;
             number_of_elevators_per_column = _amount_of_elevator_per_column;
             floor_amount = _floor_amount;
             battery_lowest_floor = _battery_lowest_floor;
+            battery_maximum_floor = _battery_maximum_floor; 
 
             for(int i = 1 ; i <= _column_amount; i++){
                 Column col = new Column(_floor_amount, _battery_lowest_floor, _floor_amount + _battery_lowest_floor, i);
                 columnList.Add(col);
             }
+
+            for(int i = 1; i <= floor_amount; i++){
+                CallButton call_button = new CallButton ("off",i);
+                callbuttonList.Add(call_button);
+            }
+
+            for(int i = 1; i<= _amount_of_elevator_per_column; i++){
+                Elevator elevator = new Elevator (i,"on",1,"none");
+                elevatorList.Add(elevator);
+            }
         }
-
-
-        public void CreateCallButtonList()
-        {
-
-        }
-
         public void RequestElevator()
         {
 
@@ -68,6 +89,7 @@ namespace Rocket_Elevator_Commercial_Controller
         public void CreateElevatorList()
         {
 
+
         }
 
         public void CreateFloorIndicatorList()
@@ -89,11 +111,18 @@ namespace Rocket_Elevator_Commercial_Controller
 
      class Elevator
     {
-        double status;
-        double current_floor;
-        double direction;
+        public string status;
+        public int current_floor;
+        public string  direction;
+        public int id;
 
 
+        public Elevator (int _id, string _elevator_status, int _elevator_current_floor, string _elevator_direction){
+            id = _id;
+            status= _elevator_status;
+            current_floor = _elevator_current_floor;
+            direction= _elevator_direction;
+        }
         public void CreateRequestList()
         {
 
@@ -108,6 +137,18 @@ namespace Rocket_Elevator_Commercial_Controller
         {
 
         }
+    }
+
+    class CallButton {
+        public string call_button_status = "off";
+        public int call_button_id;
+        
+        public CallButton(string _call_button_status, int _call_button_id){
+            call_button_status = _call_button_status;
+            call_button_id = _call_button_id;
+        }
+      
+
     }
 
 
