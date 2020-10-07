7193261c1d4c805c9028db098abb8e3eaf5ac78b
Louis-Felix Beland <louisfelix95@hotmail.com>
2020-10-07 15:53:46 -0400

    all scenarios working in go

--- Commercial_Controller.go
+++ Commercial_Controller.go
@@ -35,6 +35,60 @@ func main() {
 	bat.columnList[1].elevatorList[4].currentFloor = 6
 	fmt.Println(" SCENARIO 1")
 	bat.columnList[1].requestElevator(1, "up", 20)
+
+	// ----------SCENARIO 2----------
+	bat.columnList[2].elevatorList[0].direction = "none"
+	bat.columnList[2].elevatorList[0].currentFloor = 1
+
+	bat.columnList[2].elevatorList[1].direction = "up"
+	bat.columnList[2].elevatorList[1].currentFloor = 23
+
+	bat.columnList[2].elevatorList[2].direction = "down"
+	bat.columnList[2].elevatorList[2].currentFloor = 33
+
+	bat.columnList[2].elevatorList[3].direction = "down"
+	bat.columnList[2].elevatorList[3].currentFloor = 40
+
+	bat.columnList[2].elevatorList[4].direction = "down"
+	bat.columnList[2].elevatorList[4].currentFloor = 39
+	fmt.Println(" SCENARIO 2")
+	bat.columnList[2].requestElevator(1, "up", 36)
+
+	// ----------SCENARIO 3----------
+	bat.columnList[3].elevatorList[0].direction = "down"
+	bat.columnList[3].elevatorList[0].currentFloor = 58
+
+	bat.columnList[3].elevatorList[1].direction = "up"
+	bat.columnList[3].elevatorList[1].currentFloor = 50
+
+	bat.columnList[3].elevatorList[2].direction = "up"
+	bat.columnList[3].elevatorList[2].currentFloor = 46
+
+	bat.columnList[3].elevatorList[3].direction = "up"
+	bat.columnList[3].elevatorList[3].currentFloor = 1
+
+	bat.columnList[3].elevatorList[4].direction = "down"
+	bat.columnList[3].elevatorList[4].currentFloor = 60
+	fmt.Println(" SCENARIO 3 ")
+	bat.columnList[3].requestElevator(54, "down", 1)
+
+	// ----------SCENARIO 4----------
+	bat.columnList[0].elevatorList[0].direction = "none"
+	bat.columnList[0].elevatorList[0].currentFloor = -4
+
+	bat.columnList[0].elevatorList[1].direction = "none"
+	bat.columnList[0].elevatorList[1].currentFloor = 1
+
+	bat.columnList[0].elevatorList[2].direction = "down"
+	bat.columnList[0].elevatorList[2].currentFloor = -3
+
+	bat.columnList[0].elevatorList[3].direction = "up"
+	bat.columnList[0].elevatorList[3].currentFloor = -6
+
+	bat.columnList[0].elevatorList[4].direction = "down"
+	bat.columnList[0].elevatorList[4].currentFloor = -1
+	fmt.Println(" SCENARIO 4")
+	bat.columnList[0].requestElevator(-3, "up", 1)
 }
 
 // Battery ...
@@ -70,9 +124,10 @@ func (b *Battery) startBattery(_id int, _columnAmount int, _elevatorPerColumn in
 
 // Column ...
 type Column struct {
-	id               int
-	elevatorList     []Elevator
-	bestElevatorCase Elevator
+	id                 int
+	elevatorList       []Elevator
+	bestElevatorCase   Elevator
+	FloorIndicatorList []FloorIndicator
 }
 
 // create elevator list
@@ -80,22 +135,28 @@ func (c *Column) startColumn(_id int, _elevatorPerColumn int) {
 	c.id = _id
 
 	for i := 0; i < _elevatorPerColumn; i++ {
-		c.elevatorList = append(c.elevatorList, Elevator{i + 1, "none", 1})
+		c.elevatorList = append(c.elevatorList, Elevator{i + 1, "none", 1, "closed"})
+	}
+	// floor indicator Creation
+	for i := 1; i < len(c.elevatorList); i++ {
+		c.FloorIndicatorList = append(c.FloorIndicatorList, FloorIndicator{c.elevatorList[i].currentFloor, "on"})
 	}
 }
 
-//_________________________HERE___________________________
 // request elevator function
-//_________________________HERE___________________________
 func (c *Column) requestElevator(_requestedFloor int, _currentDirection string, _userTargetFloor int) {
 	c.findBestElevator(_requestedFloor, _currentDirection)
 	c.bestElevatorCase.moveElevator(_requestedFloor)
 	fmt.Println("ELEVATOR HAS REACHED FLOOR", _requestedFloor)
+	c.bestElevatorCase.openDoors("opened")
 	fmt.Println("OPENING DOORS....")
+	c.bestElevatorCase.openDoors("closed")
 	fmt.Println("...CLOSING DOORS")
 	c.bestElevatorCase.moveElevator(_userTargetFloor)
 	fmt.Println("ELEVATOR HAS REACHED FLOOR", _userTargetFloor)
+	c.bestElevatorCase.openDoors("opened")
 	fmt.Println("OPENING DOORS....")
+	c.bestElevatorCase.openDoors("closed")
 	fmt.Println("...CLOSING DOORS")
 }
 
@@ -145,15 +206,15 @@ func (c *Column) findBestElevator(_requestedFloor int, _currentDirection string)
 		}
 	} else {
 		for i := 0; i < len(c.elevatorList); i++ {
-			if _currentDirection == "up" && _currentDirection != c.elevatorList[i].direction && _requestedFloor >= c.elevatorList[i].currentFloor {
-				distance = c.elevatorList[i].currentFloor - _requestedFloor
+			if _currentDirection == "up" && _currentDirection == c.elevatorList[i].direction && _requestedFloor >= c.elevatorList[i].currentFloor {
+				distance = _requestedFloor - c.elevatorList[i].currentFloor
 
 				if distance < bestDistance {
 					bestDistance = distance
 					bestElevatorCase = i
 				}
-			} else if _currentDirection == "down" && _currentDirection != c.elevatorList[i].direction && _requestedFloor <= c.elevatorList[i].currentFloor {
-				distance = _requestedFloor - c.elevatorList[i].currentFloor
+			} else if _currentDirection == "down" && _currentDirection == c.elevatorList[i].direction && _requestedFloor <= c.elevatorList[i].currentFloor {
+				distance = c.elevatorList[i].currentFloor - _requestedFloor
 
 				if distance < bestDistance {
 					bestDistance = distance
@@ -190,6 +251,7 @@ type Elevator struct {
 	id           int
 	direction    string
 	currentFloor int
+	doorStatus   string
 }
 
 //move Elevator function
@@ -198,21 +260,19 @@ func (e *Elevator) moveElevator(_userTargetFloor int) {
 	_userTargetFloor = e.currentFloor
 }
 
+// openDoors function
+func (e *Elevator) openDoors(doorStatus string) {
+	doorStatus = e.doorStatus
+}
+
 // CallButton ...
 type CallButton struct {
 	id     int
 	status string
 }
 
-//FloorIndicator
-/* type FloorIndicator struct {
-
-} */
-
-/* func (cb *CallButton) startCallButton(_id int, _floorAmount int ){
-	cb.id =_id
-
-	for i := 0; i < _floorAmount; i++{
-		cb
-	}
-} */
+// FloorIndicator ...
+type FloorIndicator struct {
+	elevatorCurrentFloor int
+	floorIndicatorStatus string
+}
